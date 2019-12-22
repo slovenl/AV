ddca06e181729445a9fa60eded36c01befdca7f8
Slovenl <1009195483@qq.com>
2019-12-22 22:09:53 +0800

    libpng编译问题

--- openGLRender/src/main/jni/PngPreviewController.cpp
+++ openGLRender/src/main/jni/PngPreviewController.cpp
@@ -8,40 +8,43 @@
 
 static ANativeWindow *window = 0;
 static PicPreviewController *controller = 0;
-
+#ifdef __cplusplus
+extern "C" {
 JNIEXPORT void JNICALL Java_com_phuket_tour_opengl_renderer_PngPreviewController_init
-(JNIEnv * env, jobject obj, jstring pngFilePathParam) {
-	controller = new PicPreviewController();
-	char* pngFilePath = (char*) env->GetStringUTFChars(pngFilePathParam, NULL);
-	controller->start(pngFilePath);
-	env->ReleaseStringUTFChars(pngFilePathParam, pngFilePath);
+        (JNIEnv *env, jobject obj, jstring pngFilePathParam) {
+    controller = new PicPreviewController();
+    char *pngFilePath = (char *) env->GetStringUTFChars(pngFilePathParam, NULL);
+    controller->start(pngFilePath);
+    env->ReleaseStringUTFChars(pngFilePathParam, pngFilePath);
 }
 
 JNIEXPORT void JNICALL Java_com_phuket_tour_opengl_renderer_PngPreviewController_setSurface
-(JNIEnv * env, jobject obj, jobject surface) {
-	if (surface != 0 && NULL != controller) {
-		window = ANativeWindow_fromSurface(env, surface);
-		LOGI("Got window %p", window);
-		controller->setWindow(window);
-	} else if (window != 0) {
-		LOGI("Releasing window");
-		ANativeWindow_release(window);
-		window = 0;
-	}
+        (JNIEnv *env, jobject obj, jobject surface) {
+    if (surface != 0 && NULL != controller) {
+        window = ANativeWindow_fromSurface(env, surface);
+        LOGI("Got window %p", window);
+        controller->setWindow(window);
+    } else if (window != 0) {
+        LOGI("Releasing window");
+        ANativeWindow_release(window);
+        window = 0;
+    }
 }
 
 JNIEXPORT void JNICALL Java_com_phuket_tour_opengl_renderer_PngPreviewController_resetSize
-(JNIEnv * env, jobject obj, jint width, jint height) {
-	if (NULL != controller) {
-		controller->resetSize(width, height);
-	}
+        (JNIEnv *env, jobject obj, jint width, jint height) {
+    if (NULL != controller) {
+        controller->resetSize(width, height);
+    }
 }
 
 JNIEXPORT void JNICALL Java_com_phuket_tour_opengl_renderer_PngPreviewController_stop
-(JNIEnv * env, jobject obj) {
-	if (NULL != controller) {
-		controller->stop();
-		delete controller;
-		controller = NULL;
-	}
+        (JNIEnv *env, jobject obj) {
+    if (NULL != controller) {
+        controller->stop();
+        delete controller;
+        controller = NULL;
+    }
 }
+}
+#endif //__cplusplus
